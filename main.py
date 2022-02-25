831f7f70e3fe1ff8124fbd45f2d660b0dbb7b824
Kalvis Kincis <kalvis.kincis@r6vsk.lv>
2022-02-25 12:50:49 +0200

    Kalvis papildina programmu

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ print("Vineta Tomsone")
 print("GitHub konts: vinetat")
 print("e-pasts: vineta.tomsone@liepaja.edu.lv")
 print("Hobijs: kalnos kāpšana, kas nu jau 2 gadus nav iespējams, bet viss vēl var būt!")
+
+print("Te Kalvis kaut ko pierakstījis klāt")
